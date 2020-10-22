335631f99ab261811a241c9cc321d84d8a35f502
MSe <mse1969@posteo.de>
2020-10-22 22:59:15 +0200

    keys: Different sign method in build script
    The original method, as documented by Google / LineageOS, sometimes causes
    errors when executing the 'ota_from_target_files' python script.
    
    Therefore, the combination of PRODUCT_DEFAULT_DEV_CERTIFICATE in common.mk
    of vendor/lineage and setting a symlink testkey.* => releasekey.* in the
    certs directory to allow selinux to build has been chosen, similar to
    the docker image in github.com/lineageos4microg.
    
    Adapted for Android 9 and above:
    Soong now requires the keys directory within the build tree, hence a
    symlink is created accordingly
    
    * [pnplusplus]: Adapted for lineage-priv/keys usage also automatically
    symlink the keys from .android-certs folder and setup some .gitignore
    
    Change-Id: I273947755ffeb5beb8153d625f3a23cf4f210221
    Signed-off-by: Praditia Nur <pnplusplus@gmail.com>

---
+++ keys.mk
@@ -0,0 +1,3 @@
+$(shell ln -sf ~/.android-certs/ vendor/lineage-priv/keys/.android-certs)
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/.android-certs/releasekey
+PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage-priv/keys/.android-certs/releasekey
