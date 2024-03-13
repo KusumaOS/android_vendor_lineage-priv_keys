1cc7e19aeb6ae65ec24600df24426a1b8006f16d
Ido Ben-Hur <idoybh2@gmail.com>
2024-03-13 13:35:09 +0200

    keys: Sign every build variant but eng
    Change-Id: Iad382aa2b2242b89014f1d659fdb4e764d0cca67

--- keys.mk
+++ keys.mk
@@ -1,6 +1,8 @@
+ifneq (eng,$(TARGET_BUILD_VARIANT))
 $(shell ln -sf ~/.android-certs/ vendor/lineage-priv/keys/.android-certs)
 ifneq (,$(wildcard vendor/lineage-priv/keys/.android-certs/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/.android-certs/releasekey
 PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage-priv/keys/.android-certs/releasekey
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
 endif
